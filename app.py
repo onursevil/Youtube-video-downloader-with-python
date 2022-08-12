89ad526f5754a9d271c1368d4d66197a280410e9
onursevil <88597920+onursevil@users.noreply.github.com>
2022-08-12 12:10:55 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from pytube import YouTube
+
+app=Tk()
+app.geometry("800x370");app.configure(bg="grey");app.resizable(False,False)
+Label(app,text="Link : ",font="impack 15 bold",bg="grey",fg="black").place(x=40,y=130)
+
+def download():
+     link=YouTube(str(entry.get()))
+     dload=link.streams.first()
+     dload.download("C:\\Users\\huawei\\Desktop")
+
+entry=Entry(app,width=60,bd=7,font="impack 10 bold",justify="center")
+entry.place(x=220,y=130)
+
+
+
+Button(app,text="Download",font="impack 20 bold",bg="red",fg="white",command=download).place(x=350,y=200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mainloop()
