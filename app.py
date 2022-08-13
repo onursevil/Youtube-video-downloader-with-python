ba8a207dd6a1948859cdd06b57a1fe9577b36880
onursevil <88597920+onursevil@users.noreply.github.com>
2022-08-13 18:55:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Label(app,text="Link : ",font="impack 15 bold",bg="grey",fg="black").place(x=40,
 def download():
      link=YouTube(str(entry.get()))
      dload=link.streams.first()
-     dload.download("C:\\Users\\huawei\\Desktop")
+     dload.download("C:\\Users\\huawei\\Desktop")  ##Make sure that you have writen correct directory for your computer.
 
 entry=Entry(app,width=60,bd=7,font="impack 10 bold",justify="center")
 entry.place(x=220,y=130)
